e3caf50a0fb9518314391225e8aff7bf506c2187
ShinyAri <tanhngusi39@gmail.com>
2024-10-30 15:42:48 +0100

    Update: update benefit

---
+++ frontend/src/components/Benefit.tsx
@@ -0,0 +1,77 @@
+import { Box, Typography } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faComments,
+  faPlane,
+  faDollarSign,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Benefit = () => {
+  return (
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      sx={{
+        backgroundColor: "rgb(234, 234, 234)",
+        borderRadius: "20px",
+        margin: "40px 80px",
+        color: "black",
+      }}
+      gap="40px"
+    >
+      <Box
+        bgcolor="white"
+        sx={{ flex: "1", borderRadius: "8px" }}
+        padding="10px 20px"
+        display="flex"
+        flexDirection="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <FontAwesomeIcon icon={faComments} fontSize="2.3rem" />
+        <Box marginLeft="20px">
+          <Typography fontSize="1.4rem" fontWeight="400">
+            We are Now Available
+          </Typography>
+          <Typography>Call +1 555 666 888 contact with us</Typography>
+        </Box>
+      </Box>
+      <Box
+        bgcolor="white"
+        sx={{ flex: "1", borderRadius: "8px" }}
+        padding="10px 20px"
+        display="flex"
+        flexDirection="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <FontAwesomeIcon icon={faPlane} fontSize="2.3rem" />
+        <Box marginLeft="20px">
+          <Typography fontSize="1.4rem" fontWeight="400">
+            International Flight
+          </Typography>
+          <Typography>Call +1 555 666 888 contact with us</Typography>
+        </Box>
+      </Box>
+      <Box
+        bgcolor="white"
+        sx={{ flex: "1", borderRadius: "8px" }}
+        padding="10px 20px"
+        display="flex"
+        flexDirection="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <FontAwesomeIcon icon={faDollarSign} fontSize="2.3rem" />
+        <Box marginLeft="20px">
+          <Typography fontSize="1.4rem" fontWeight="400">
+            Check Refund
+          </Typography>
+          <Typography>Call +1 555 666 888 contact with us</Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Benefit;
--- frontend/src/pages/home-page.tsx
+++ frontend/src/pages/home-page.tsx
@@ -12,13 +12,17 @@ import "../styles.css";
 import Header from "components/Header";
 import Banner from "components/Banner";
 import FlightSearch from "components/FlightSearch";
+import Benefit from "components/Benefit";
 
 const HomePage = () => {
   return (
     <Box>
       <Header />
-      <Banner />
-      <FlightSearch />
+      <Box sx={{ bgcolor: "rgb(234, 234, 234)" }}>
+        <Banner />
+        <FlightSearch />
+        <Benefit />
+      </Box>
     </Box>
   );
 };
