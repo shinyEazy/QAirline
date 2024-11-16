ae261129077128e33df6b5ed1524db17950c77ae
ShinyAri <tanhngusi39@gmail.com>
2024-11-16 08:35:54 +0000

    Update: enhance Payment page layout and functionality; add Step and Pay components

---
+++ frontend/src/components/payment/Step.tsx
@@ -0,0 +1,66 @@
+import { Box, Typography, Button } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+
+const Step = () => {
+  const navigate = useNavigate();
+  return (
+    <Box
+      sx={{
+        backgroundColor: "white",
+        borderRadius: "20px",
+        padding: "20px",
+        color: "black",
+        boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
+        display: "flex",
+        gap: "24px",
+      }}
+    >
+      <Box width="100%" display="flex" alignItems="center">
+        <FontAwesomeIcon
+          onClick={() => navigate("/flight-listing")}
+          icon={faCircleCheck}
+          style={{ color: "rgb(77,115,252)", fontSize: "40px" }}
+        />
+        <Typography variant="h6" sx={{ marginLeft: 1 }}>
+          Your Selection
+        </Typography>
+      </Box>
+      <Box width="100%" display="flex" alignItems="center">
+        <FontAwesomeIcon
+          onClick={() => navigate("/flight-booking")}
+          icon={faCircleCheck}
+          style={{ color: "rgb(77,115,252)", fontSize: "40px" }}
+        />
+        <Typography variant="h6" sx={{ marginLeft: 1 }}>
+          Your Details
+        </Typography>
+      </Box>
+      <Box width="100%" display="flex" alignItems="center">
+        <Button
+          sx={{
+            fontSize: "1.3rem",
+            backgroundColor: "rgb(77,115,252)",
+            color: "white",
+            borderRadius: "50%",
+            minWidth: "40px",
+            height: "40px",
+            padding: 0,
+            "&:hover": {
+              backgroundColor: "rgb(77,115,252)",
+              boxShadow: "none",
+            },
+          }}
+        >
+          3
+        </Button>
+        <Typography variant="h6" sx={{ marginLeft: 1 }}>
+          Payment
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default Step;
--- frontend/src/pages/Payment.tsx
+++ frontend/src/pages/Payment.tsx
@@ -1,12 +1,51 @@
-import { Box } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 import Header from "components/home-page/Header";
 import Footer from "components/home-page/Footer";
+import Step from "components/payment/Step";
+import BookingDetail from "components/flight-booking/BookingDetail";
+import Price from "components/flight-booking/Price";
+import Pay from "components/payment/Pay";
 
 const Payment = () => {
   return (
     <Box>
       <Header />
-      <Box>Payment</Box>
+      <Typography
+        display="flex"
+        width="100%"
+        alignItems="center"
+        textAlign="center"
+        justifyContent="center"
+        height="300px"
+      >
+        Flight Booking
+      </Typography>
+
+      <Box
+        display="flex"
+        gap="40px"
+        sx={{
+          margin: "20px 80px 80px",
+        }}
+      >
+        <Box
+          sx={{
+            flexBasis: "60%",
+          }}
+        >
+          <Step />
+          <Pay />
+        </Box>
+        <Box
+          sx={{
+            flexBasis: "40%",
+            minWidth: "250px",
+          }}
+        >
+          <BookingDetail />
+          <Price />
+        </Box>
+      </Box>
       <Footer />
     </Box>
   );
