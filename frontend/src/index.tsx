c14d4b27a0973a57ab42045e2c3e243b8c907c8b
ShinyAri <tanhngusi39@gmail.com>
2024-12-09 07:41:38 +0700

    Update: update web with new Font

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
+const theme = createTheme({
+  typography: {
+    fontFamily: "JetBrains Mono, monospace",
+  },
+});
+
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ThemeProvider>
 );
